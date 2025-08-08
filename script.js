78bdd10b608362b02c059c0bfa2dd5c7759bf453
DeekshithGA <venmathi.g29@gmail.com>
2025-08-08 18:20:41 +0530

    website

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    const rotateX = -y / 20;
+    const rotateY = x / 20;
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
+});
